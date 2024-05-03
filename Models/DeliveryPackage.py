3dfb892d80ead406e721967839b461b663228614
Diljot Singh <sdiljot291@gmail.com>
2024-05-03 10:23:46 -0700

    create Package class and Truck class to store data

--- Models/DeliveryPackage.py
+++ Models/DeliveryPackage.py
@@ -1,6 +1,10 @@
-
+# the class Package is to store the information of a Package like
+# it's id, address, city, state zip, package's delivery deadline time, weight of package,
+# current status of the package and
+# delivery_time, the time it was delivered (only updates after delivered), initially set to "At the hub"
+# and last the truck_id, the id of truck it is loaded in or was loaded in
 class Package:
-    def __init__(self, p_id, address, city, state, zip, delivery_deadline, weight, status):
+    def __init__(self, p_id, address, city, state, zip, delivery_deadline, weight, status, delivery_time, truck_id):
         self.p_id = p_id
         self.address = address
         self.city = city
@@ -9,8 +13,11 @@ class Package:
         self.delivery_deadline = delivery_deadline
         self.weight = weight
         self.status = status
+        self.delivery_time = delivery_time
+        self.truck_id = truck_id
 
+    # the __str__ function is to simply print the Package object and it's properties
     def __str__(self):
-        return "%s, %s, %s, %s, %s, %s, %s, %s" % (self.p_id, self.address, self.city, self.state, self.zip,
+        return "%s, %s, %s, %s, %s, %s, %s, %s, %s" % (self.p_id, self.address, self.city, self.state, self.zip,
                                                    self.delivery_deadline, self.weight,
-                                                   self.status)
+                                                   self.status, self.delivery_time)
--- Models/Truck.py
+++ Models/Truck.py
@@ -1,7 +1,19 @@
 
-class Truck:
-    def __init__(self, milesTravelled=0, time=0, capacity=0):
-        self.milesTravelled = milesTravelled
-        self.time = time
-        self.capacity = capacity
+# The class Truck creates a truck object with properties id, truck's capacity, speed, packages it has to deliver,
+# depart time of the truck, miles travelled by the truck and the current time of the
+# truck with respect to the packages it has delivered
 
+class Truck:
+    def __init__(self, id, capacity, speed, packages, depart_time, miles_travelled, time):
+        self.id = id
+        self.capacity = capacity
+        self.speed = speed
+        self.packages = packages
+        self.depart_time = depart_time
+        self.miles_travelled = miles_travelled
+        self.time = time
+
+    # the __str__ function is to simply print the truck object and it's properties
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s" % (self.id, self.capacity, self.speed, self.packages,
+                                               self.depart_time, self.miles_travelled, self.time)
